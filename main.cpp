1b49ea1c7b98d66a276822075f3633eed304e80c
ksam1723 <kevinsam68@gmail.com>
2025-09-24 22:22:49 -0400

    Completed userMemoryAllocation

--- main.cpp
+++ main.cpp
@@ -1,16 +1,91 @@
+#include <iomanip>
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <cmath>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+struct Process {
+    int pId;
+    long long startAddress;
+    long long sizeMb;
+    long long page;
+    long long unusedMb;
+};
+
+// Sim class
+using namespace std;
+class MemorySim {
+private:
+    static const int totalPages = 100;
+    static const int pageSizeMb = 160;
+    static const int stAddress= 2000;
+
+    int pages;
+    long long currentAddress;
+    vector<Process> processes;
+
+public:
+    // Constructor
+    MemorySim() {
+        pages = 0;
+        currentAddress = stAddress;
+        srand(time(0));
     }
 
+    void userMemoryAllocation() {
+        int pId = 1;
+
+        while (pages < totalPages) {
+            int randNum = rand() % 30 + 1;
+            long long processSize = randNum * 80;
+            int pages2 = ceil(((double) processSize) / pageSizeMb);
+
+            // If there are no pages left
+            if (pages + pages2 > totalPages) {
+                break;
+            }
+
+            long long allocatedSpace = pages2 * pageSizeMb;
+            long long unusedSpace = allocatedSpace - processSize;
+
+            Process p;
+            p.pId = pId++;
+            p.startAddress = currentAddress;
+            p.sizeMb = processSize;
+            p.page = pages2;
+            p.unusedMb = unusedSpace;
+
+            processes.push_back(p);
+
+            // Update memory
+            pages += pages2;
+            currentAddress += allocatedSpace;
+        }
+    }
+
+    void print() {
+        cout << left << setw(15) << "Process Id"
+        << setw(25) << "Starting Memory Address"
+        << setw(20) << "Process Size (Mb)"
+        << setw(20) << "Unused Space (Mb)" << endl;
+
+        cout << string(90, '-' ) << endl;
+
+        for (auto p : processes) {
+            cout << left << setw(15) << p.pId
+            << setw(25) << p.startAddress
+            << setw(20) << p.sizeMb
+            << setw(20) << p.unusedMb << endl;
+        }
+    }
+};
+
+
+using namespace std;
+int main() {
+    MemorySim sim;
+    sim.userMemoryAllocation();
+    sim.print();
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
